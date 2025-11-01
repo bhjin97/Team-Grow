3fbcd9f0375b68313773128a37efa759844ac5a2
bhjin97 <baehj0907@gmail.com>
2025-11-01 10:23:28 +0900

    스트림릿 오류 고침(DB 컬럼명 바뀌면서 생긴 오류)

--- aller_app_mariadb/aller/profiles.py
+++ aller_app_mariadb/aller/profiles.py
@@ -1,20 +1,20 @@
 from sqlalchemy import text
 from .storage_sql import get_engine
 
-def upsert_profile(user_id: int, nickname=None, birth_year=None,
+def upsert_profile(user_id: int, nickname=None, birth_date=None,
                    gender="na", skin_type_code=None,
                    skin_axes_json=None, preferences_json=None,
                    allergies_json=None):
     q = text("""
     INSERT INTO user_profiles
-      (user_id, nickname, birth_year, gender,
+      (user_id, nickname, birth_date, gender,
        skin_type_code, skin_axes_json, preferences_json, allergies_json, last_quiz_at)
     VALUES
       (:uid, :nickname, :byear, :gender,
        :code, :axes, :prefs, :allergies, NOW())
     ON DUPLICATE KEY UPDATE
       nickname=VALUES(nickname),
-      birth_year=VALUES(birth_year),
+      birth_date=VALUES(birth_date),
       gender=VALUES(gender),
       skin_type_code=VALUES(skin_type_code),
       skin_axes_json=VALUES(skin_axes_json),
@@ -25,7 +25,7 @@ def upsert_profile(user_id: int, nickname=None, birth_year=None,
     """)
     with get_engine().begin() as conn:
         conn.execute(q, {
-            "uid": user_id, "nickname": nickname, "byear": birth_year, "gender": gender,
+            "uid": user_id, "nickname": nickname, "byear": birth_date, "gender": gender,
             "code": skin_type_code, "axes": skin_axes_json,
             "prefs": preferences_json, "allergies": allergies_json
         })
@@ -33,7 +33,7 @@ def upsert_profile(user_id: int, nickname=None, birth_year=None,
 def get_profile(user_id: int):
     with get_engine().connect() as conn:
         return conn.execute(text("""
-            SELECT user_id, nickname, birth_year, gender,
+            SELECT user_id, nickname, birth_date, gender,
                    skin_type_code, skin_axes_json, preferences_json, allergies_json, last_quiz_at
             FROM user_profiles WHERE user_id = :uid
         """), {"uid": user_id}).mappings().fetchone()
